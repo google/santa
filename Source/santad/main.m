44c9d9aead3277dc78d55e275988972a8933f8b7
Russell Hancox <russell@hancox.us>
2015-06-15 16:31:55 -0400

    santad: Add watchdog thread to print warnings if CPU/RAM usage seem high.

--- Source/santad/main.m
+++ Source/santad/main.m
@@ -14,7 +14,62 @@
 
 #include "SNTLogging.h"
 
+#include <pthread/pthread.h>
+#include <sys/resource.h>
+
 #import "SNTApplication.h"
+
+///  Converts a timeval struct to double, converting the microseconds value to seconds.
+static inline double timeval_to_double(struct timeval tv) {
+  return (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
+}
+
+///  The watchdog thread function, used to monitor santad CPU/RAM usage and print a warning
+///  if it goes over certain thresholds.
+void *watchdog_thread_f(__unused void *idata) {
+  pthread_setname_np("Watchdog");
+
+  // Number of seconds to wait between checks.
+  const int timeInterval = 30;
+
+  // Amount of CPU usage to trigger warning, as a percentage averaged over timeInterval
+  // santad's usual CPU usage is 0-3% but can occasionally spike if lots of processes start at once.
+  const int cpuWarnThreshold = 10;
+
+  // Amount of RAM usage to trigger warning, in MB.
+  // santad's usual RAM usage is between 5-50MB but can spike if lots of processes start at once.
+  const int memWarnThreshold = 100;
+
+  struct rusage usage;
+  static double prev_total_time = 0.0;
+  struct mach_task_basic_info t_info;
+  mach_msg_type_number_t t_info_count = MACH_TASK_BASIC_INFO_COUNT;
+
+  while(true) {
+    sleep(timeInterval);
+
+    // CPU
+    getrusage(RUSAGE_SELF, &usage);
+    double total_time = timeval_to_double(usage.ru_utime) + timeval_to_double(usage.ru_stime);
+    double percentage = (((total_time - prev_total_time) / (double)timeInterval) * 100.0);
+    prev_total_time = total_time;
+
+    if (percentage > cpuWarnThreshold) {
+      LOGW(@"Watchdog: potentially high CPU use, ~%.2f%% over last %d seconds.",
+           percentage, timeInterval);
+    }
+
+    // RAM
+    if (KERN_SUCCESS == task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
+                                  (task_info_t)&t_info, &t_info_count)) {
+      double ramUseMb = (double) t_info.resident_size / 1024 / 1024;
+      if (ramUseMb > (double)memWarnThreshold) {
+        LOGW(@"Watchdog: potentially high RAM use, RSS is %.2fMB.", ramUseMb);
+      }
+    }
+  }
+  return NULL;
+}
 
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
@@ -32,6 +87,10 @@ int main(int argc, const char *argv[]) {
 
     SNTApplication *s = [[SNTApplication alloc] init];
     [s performSelectorInBackground:@selector(run) withObject:nil];
+
+    // Create watchdog thread
+    pthread_t watchdog_thread;
+    pthread_create(&watchdog_thread, NULL, watchdog_thread_f, NULL);
 
     [[NSRunLoop mainRunLoop] run];
   }
