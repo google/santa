5fda5bc081a58f51b03bb733d1c6b47cac8e36d4
Russell Hancox <russell@hancox.us>
2015-10-29 12:35:27 -0400

    santactl/binaryinfo: Only print bundle lines if bundle info is present

--- Source/santactl/binaryinfo/SNTCommandBinaryInfo.m
+++ Source/santactl/binaryinfo/SNTCommandBinaryInfo.m
@@ -65,9 +65,12 @@ REGISTER_COMMAND_NAME(@"binaryinfo")
   [self printKey:@"Path" value:fileInfo.path];
   [self printKey:@"SHA-256" value:fileInfo.SHA256];
   [self printKey:@"SHA-1" value:fileInfo.SHA1];
-  [self printKey:@"Bundle Name" value:fileInfo.bundleName];
-  [self printKey:@"Bundle Version" value:fileInfo.bundleVersion];
-  [self printKey:@"Bundle Version Str" value:fileInfo.bundleShortVersionString];
+
+  if (fileInfo.bundlePath) {
+    [self printKey:@"Bundle Name" value:fileInfo.bundleName];
+    [self printKey:@"Bundle Version" value:fileInfo.bundleVersion];
+    [self printKey:@"Bundle Version Str" value:fileInfo.bundleShortVersionString];
+  }
 
   if (fileInfo.quarantineDataURL) {
     [self printKey:@"Download Referer URL" value:fileInfo.quarantineRefererURL];
