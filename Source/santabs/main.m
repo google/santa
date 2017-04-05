723fe84e5346045fe262754dc975794819c58c93
tburgin <tom.burgin@mac.com>
2017-04-05 15:46:00 -0400

    post rebase updates

--- Source/santabs/main.m
+++ Source/santabs/main.m
@@ -12,6 +12,8 @@
 ///    See the License for the specific language governing permissions and
 ///    limitations under the License.
 
+@import Foundation;
+
 #import "SNTBundleService.h"
 #import "SNTXPCBundleServiceInterface.h"
 #import "SNTXPCConnection.h"
