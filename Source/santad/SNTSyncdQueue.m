64eea7f98e2362c2362be1857f58ae48d4b578a7
nguyen-phillip <nguyenphillip@google.com>
2017-09-13 18:31:58 -0400

    fix indentation

--- Source/santad/SNTSyncdQueue.m
+++ Source/santad/SNTSyncdQueue.m
@@ -54,13 +54,13 @@
   [self dispatchBlockOnSyncdQueue:^{
     [self.syncdConnection.remoteObjectProxy
         postBundleEventToSyncServer:event reply:^(SNTBundleEventAction action) {
-          // Remove the backoff entry for the inital block event. The same event will be included in
-          // the related events synced using addEvents:isFromBundle:.
-          if (action == SNTBundleEventActionSendEvents) {
-            [self.uploadBackoff removeObjectForKey:event.fileBundleHash];
-          }
-          reply(action);
-        }];
+      // Remove the backoff entry for the inital block event. The same event will be included in
+      // the related events synced using addEvents:isFromBundle:.
+      if (action == SNTBundleEventActionSendEvents) {
+        [self.uploadBackoff removeObjectForKey:event.fileBundleHash];
+      }
+      reply(action);
+    }];
   }];
 }
 
