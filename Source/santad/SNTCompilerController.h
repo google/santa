8f972ebb05df1f09fcb0e26eee96e81719043990
nguyen-phillip <nguyenphillip@google.com>
2017-11-02 15:45:58 -0400

    prototype code for santa transitive whitelisting

---
+++ Source/santad/SNTCompilerController.h
@@ -0,0 +1,13 @@
+//
+//  SNTCompilerController.h
+//  Santa
+//
+//  Created by Phillip Nguyen on 10/18/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SNTCompilerController : NSObject
+
+@end
---
+++ Source/santad/SNTCompilerController.m
@@ -0,0 +1,13 @@
+//
+//  SNTCompilerController.m
+//  Santa
+//
+//  Created by Phillip Nguyen on 10/18/17.
+//
+//
+
+#import "SNTCompilerController.h"
+
+@implementation SNTCompilerController
+
+@end
