880170ea7d44c37d264058e9ad1d1727e8e28e90
Tom Burgin <tburgin@users.noreply.github.com>
2019-09-27 15:10:37 -0400

    make santabundleservice a command line app (#395)
    * make santabundleservice a command line app
    
    * bazel - don't build santabs.xpc

--- Source/santabundleservice/SNTBundleService.m
+++ Source/santabundleservice/SNTBundleService.m
@@ -12,7 +12,7 @@
 ///    See the License for the specific language governing permissions and
 ///    limitations under the License.
 
-#import "Source/santabs/SNTBundleService.h"
+#import "Source/santabundleservice/SNTBundleService.h"
 
 #include <stdatomic.h>
 
