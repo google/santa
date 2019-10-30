976129a202c82c1e99eb714b46e2b95ae4b325ca
Russell Hancox <russell@hancox.us>
2019-10-30 13:56:57 -0400

    santad: Re-work targets to avoid unnecessary postprocessing

--- Source/santad/postprocess.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Move the santad binary to com.google.santa.daemon
-mv "${1}/com.google.santa.daemon.systemextension/Contents/MacOS/santad" \
-   "${1}/com.google.santa.daemon.systemextension/Contents/MacOS/com.google.santa.daemon"
