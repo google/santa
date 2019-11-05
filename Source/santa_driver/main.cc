3d393e9aa4d31f3244d45545290bccbe75888860
Russell Hancox <russell@hancox.us>
2019-11-05 13:26:06 -0500

    santa-driver: Workaround 10.15 SDK Dispatch() issue

--- Source/santa_driver/main.cc
+++ Source/santa_driver/main.cc
@@ -16,3 +16,14 @@ extern "C" {
 
   __private_extern__ int _kext_apple_cc = __APPLE_CC__ ;
 }
+
+#include <IOKit/IOService.h>
+#include <IOKit/IOUserClient.h>
+
+// The macOS 10.15 SDK added these Dispatch methods but they aren't
+// available on older macOS versions and so prevent kext linking.
+// Defining them here as hidden weak no-op's fixes linking and seems to work.
+kern_return_t __attribute__((visibility("hidden"))) __attribute__((weak)) OSMetaClassBase::Dispatch(const IORPC rpc) { return KERN_SUCCESS; }
+kern_return_t __attribute__((visibility("hidden"))) __attribute__((weak)) OSObject::Dispatch(const IORPC rpc) { return KERN_SUCCESS; }
+kern_return_t __attribute__((visibility("hidden"))) __attribute__((weak)) IOService::Dispatch(const IORPC rpc) { return KERN_SUCCESS; }
+kern_return_t __attribute__((visibility("hidden"))) __attribute__((weak)) IOUserClient::Dispatch(const IORPC rpc) { return KERN_SUCCESS; }
