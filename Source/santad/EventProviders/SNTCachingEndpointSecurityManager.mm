92a9013c485842168054815b7986914c22c9499d
Russell Hancox <russell@hancox.us>
2021-01-28 10:20:45 -0500

    santad: Use OS_FALLTHROUGH

--- Source/santad/EventProviders/SNTCachingEndpointSecurityManager.mm
+++ Source/santad/EventProviders/SNTCachingEndpointSecurityManager.mm
@@ -98,7 +98,7 @@ template<> uint64_t SantaCacheHasher<santa_vnode_id_t>(santa_vnode_id_t const& t
       break;
     case ACTION_RESPOND_DENY:
       [self addToCache:sm.vnode_id decision:ACTION_RESPOND_DENY timeout:GetCurrentUptime()];
-      // fallthrough
+      OS_FALLTHROUGH;
     case ACTION_RESPOND_TOOLONG:
       ret = es_respond_auth_result(self.client, (es_message_t *)sm.es_message,
                                    ES_AUTH_RESULT_DENY, false);
