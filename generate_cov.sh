c3276dfedb73a28b77f2914c19418128fdbbd281
Kent Ma <tnek@google.com>
2021-08-09 11:40:57 -0400

    Add coveralls CI rule and lcov generator

---
+++ generate_cov.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+GIT_ROOT=$(git rev-parse --show-toplevel)
+PROFILE_PATH="$GIT_ROOT/CoverageData"
+COV_FILE="$PROFILE_PATH/info.lcov"
+
+function build() {
+    bazel coverage \
+            --test_env="LLVM_PROFILE_FILE=$PROFILE_PATH/default.profraw" \
+            --experimental_use_llvm_covmap \
+            --spawn_strategy=standalone \
+            --cache_test_results=no \
+            --test_env=LCOV_MERGER=/usr/bin/true \
+            --define=SANTA_BUILD_TYPE=ci \
+            //:unit_tests
+
+    xcrun llvm-profdata merge "$PROFILE_PATH/default.profraw" -output "$PROFILE_PATH/default.profdata"
+}
+
+function generate_lcov() {
+    object_files=$(find -L $(bazel info bazel-bin)  -type f -exec file -L {} \; | grep "Mach-O" | sed 's,:.*,,')
+
+    true > $COV_FILE
+    for file in $object_files; do
+        xcrun llvm-cov export -instr-profile "$PROFILE_PATH/default.profdata" -format=lcov \
+          $file >> $COV_FILE
+    done
+}
+
+function main() {
+    mkdir -p $PROFILE_PATH
+    build
+    generate_lcov
+}
+main
