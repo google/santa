796109cc6023875152f3359d05632497e4defd9d
Kent Ma <tnek@google.com>
2021-08-17 14:26:09 -0400

    Adding cert rules and testdata (#578)

---
+++ Source/santad/testdata/binaryrules/noop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf(
+      "There should be no explciit rules in either direction for the execution "
+      "of "
+      "this binary");
+  return 0;
+}
