d6b1944646b6cad161cf432b28a43a6028172a53
Pete Markowsky <markowsky@google.com>
2021-09-23 09:23:14 -0400

    Initial commit of an HTTP writer for SNTMetricSets.
    This PR adds support for shipping serialized SNTMetricSets to an HTTP server via POSTs.

--- Source/santametricservice/SNTMetricServiceTest.m
+++ Source/santametricservice/SNTMetricServiceTest.m
@@ -5,6 +5,7 @@
 #import "Source/common/SNTConfigurator.h"
 #import "Source/common/SNTMetricSet.h"
 
+#import <MOLAuthenticatingURLSession/MOLAuthenticatingURLSession.h>
 #import <OCMock/OCMock.h>
 
 #import "Source/santametricservice/Formats/SNTMetricFormatTestHelper.h"
@@ -16,6 +17,9 @@ NSDictionary *validMetricsDict = nil;
 @property id mockConfigurator;
 @property NSString *tempDir;
 @property NSURL *jsonURL;
+@property id mockSession;
+@property id mockSessionDataTask;
+@property id mockMOLAuthenticatingURLSession;
 @end
 
 @implementation SNTMetricServiceTest
@@ -39,6 +43,15 @@ NSDictionary *validMetricsDict = nil;
 
 - (void)tearDown {
   [self.mockConfigurator stopMocking];
+  if (self.mockSessionDataTask != nil) {
+    [self.mockSessionDataTask stopMocking];
+  }
+  if (self.mockSession != nil) {
+    [self.mockSession stopMocking];
+  }
+  if (self.mockMOLAuthenticatingURLSession != nil) {
+    [self.mockMOLAuthenticatingURLSession stopMocking];
+  }
 
   // delete the temp dir
   [[NSFileManager defaultManager] removeItemAtPath:self.tempDir error:NULL];
@@ -111,5 +124,52 @@ NSDictionary *validMetricsDict = nil;
   [self convertJSONDateStringsToNSDateWithJson:parsedJSONData];
 
   XCTAssertEqualObjects(validMetricsDict, parsedJSONData, @"invalid JSON created");
+}
+
+- (void)testWritingJSON {
+  NSURL *url = [NSURL URLWithString:@"http://localhost:9444"];
+  OCMStub([self.mockConfigurator exportMetrics]).andReturn(YES);
+  OCMStub([self.mockConfigurator metricFormat]).andReturn(SNTMetricFormatTypeRawJSON);
+  OCMStub([self.mockConfigurator metricURL]).andReturn(url);
+
+  self.mockSession = OCMClassMock([NSURLSession class]);
+  self.mockSessionDataTask = OCMClassMock([NSURLSessionDataTask class]);
+  self.mockMOLAuthenticatingURLSession = OCMClassMock([MOLAuthenticatingURLSession class]);
+
+  OCMStub([self.mockMOLAuthenticatingURLSession alloc])
+    .andReturn(self.mockMOLAuthenticatingURLSession);
+  OCMStub([self.mockMOLAuthenticatingURLSession session]).andReturn(self.mockSession);
+
+  NSHTTPURLResponse *response =
+    [[NSHTTPURLResponse alloc] initWithURL:url
+                                statusCode:200
+                               HTTPVersion:@"HTTP/1.1"
+                              headerFields:@{@"content-type" : @"application/json"}];
+
+  __block void (^passedBlock)(NSData *, NSURLResponse *, NSError *);
+
+  XCTestExpectation *responseCallback =
+    [[XCTestExpectation alloc] initWithDescription:@"ensure writer passed JSON"];
+
+  // stub out session to call completion handler immediately.
+  void (^callCompletionHandler)(NSInvocation *) = ^(NSInvocation *invocation) {
+    passedBlock(nil, response, nil);
+    [responseCallback fulfill];
+  };
+
+  OCMStub([(NSURLSessionDataTask *)self.mockSessionDataTask resume]).andDo(callCompletionHandler);
+
+  // stub out NSURLSession to assign our completion handler and return our mock
+  void (^getCompletionHandler)(NSInvocation *) = ^(NSInvocation *invocation) {
+    [invocation getArgument:&passedBlock atIndex:3];
+  };
+
+  OCMStub([self.mockSession dataTaskWithRequest:[OCMArg any] completionHandler:[OCMArg any]])
+    .andDo(getCompletionHandler)
+    .andReturn(self.mockSessionDataTask);
+
+  SNTMetricService *service = [[SNTMetricService alloc] init];
+  [service exportForMonitoring:[SNTMetricFormatTestHelper createValidMetricsDictionary]];
+  [self waitForExpectations:@[ responseCallback ] timeout:10.0];
 }
 @end
---
+++ Source/santametricservice/Writers/SNTMetricHTTPWriterTest.m
@@ -0,0 +1,186 @@
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+#import <MOLAuthenticatingURLSession/MOLAuthenticatingURLSession.h>
+#import <OCMock/OCMock.h>
+
+#import "Source/santametricservice/Writers/SNTMetricHTTPWriter.h"
+
+@interface SNTMetricHTTPWriterTest : XCTestCase
+@property id mockSession;
+@property id mockSessionDataTask;
+@property id mockMOLAuthenticatingURLSession;
+@property NSMutableArray<NSDictionary *> *mockResponses;
+@property SNTMetricHTTPWriter *httpWriter;
+@end
+
+@implementation SNTMetricHTTPWriterTest
+
+- (void)setUp {
+  self.mockSession = OCMClassMock([NSURLSession class]);
+  self.mockSessionDataTask = OCMClassMock(
+    [NSURLSessionDataTask class]);  //)[OCMockObject niceMockForClass:[NSURLSessionDataTask class]];
+  self.mockMOLAuthenticatingURLSession = OCMClassMock([MOLAuthenticatingURLSession class]);
+
+  OCMStub([self.mockMOLAuthenticatingURLSession alloc])
+    .andReturn(self.mockMOLAuthenticatingURLSession);
+  OCMStub([self.mockMOLAuthenticatingURLSession session]).andReturn(self.mockSession);
+
+  self.httpWriter = [[SNTMetricHTTPWriter alloc] init];
+  self.mockResponses = [[NSMutableArray alloc] init];
+
+  __block void (^completionHandler)(NSData *, NSURLResponse *, NSError *);
+
+  void (^getCompletionHandler)(NSInvocation *) = ^(NSInvocation *invocation) {
+    [invocation getArgument:&completionHandler atIndex:3];
+  };
+
+  void (^callCompletionHandler)(NSInvocation *) = ^(NSInvocation *invocation) {
+    NSDictionary *responseValue = self.mockResponses[0];
+    if (responseValue != nil) {
+      completionHandler(responseValue[@"data"], responseValue[@"response"],
+                        responseValue[@"error"]);
+      [self.mockResponses removeObjectAtIndex:0];
+    } else {
+      XCTFail(@"mockResponses set to zero");
+    }
+  };
+
+  OCMStub([(NSURLSessionDataTask *)self.mockSessionDataTask resume]).andDo(callCompletionHandler);
+
+  OCMStub([self.mockSession dataTaskWithRequest:[OCMArg any] completionHandler:[OCMArg any]])
+    .andDo(getCompletionHandler)
+    .andReturn(self.mockSessionDataTask);
+}
+
+- (void)tearDown {
+  [self.mockSessionDataTask stopMocking];
+  [self.mockSession stopMocking];
+  [self.mockMOLAuthenticatingURLSession stopMocking];
+}
+
+/// enqueues a mock HTTP response for testing.
+- (void)createMockResponseWithURL:(NSURL *)url
+                         withCode:(NSInteger)code
+                         withData:(NSData *)data
+                        withError:(NSError *)err {
+  NSHTTPURLResponse *response =
+    [[NSHTTPURLResponse alloc] initWithURL:url
+                                statusCode:code
+                               HTTPVersion:@"HTTP/1.1"
+                              headerFields:@{@"content-type" : @"application/json"}];
+
+  NSMutableDictionary *responseValue = [[NSMutableDictionary alloc] init];
+
+  responseValue[@"data"] = data;
+  responseValue[@"response"] = response;
+  responseValue[@"error"] = err;
+
+  [self.mockResponses addObject:responseValue];
+}
+
+- (void)testValidPostOfData {
+  NSURL *url = [[NSURL alloc] initWithString:@"http://localhost:8444/submit"];
+
+  [self createMockResponseWithURL:url withCode:200 withData:nil withError:nil];
+
+  SNTMetricHTTPWriter *httpWriter = [[SNTMetricHTTPWriter alloc] init];
+
+  NSData *JSONdata = [@"{\"foo\": \"bar\"}\r\n" dataUsingEncoding:NSUTF8StringEncoding];
+
+  NSError *err;
+  BOOL result = [httpWriter write:@[ JSONdata ] toURL:url error:&err];
+  XCTAssertEqual(YES, result);
+  XCTAssertNil(err);
+}
+
+- (void)testEnsureHTTPErrorCodesResultInErrors {
+  NSURL *url = [NSURL URLWithString:@"http://localhost:10444"];
+
+  NSData *JSONdata = [@"{\"foo\": \"bar\"}\r\n" dataUsingEncoding:NSUTF8StringEncoding];
+  NSError *err;
+
+  for (NSInteger code = 400; code < 600; code += 100) {
+    [self createMockResponseWithURL:url withCode:code withData:nil withError:nil];
+
+    BOOL result = [self.httpWriter write:@[ JSONdata ] toURL:url error:&err];
+
+    XCTAssertEqual(NO, result, @"result of call to write did not fail as expected");
+    XCTAssertNotNil(err);
+    XCTAssertEqual(code, err.code);
+    XCTAssertEqual(@"com.google.santa.metricservice.writers.http", err.domain);
+
+    NSString *expectedErrMsg = [NSString
+      stringWithFormat:@"received http status code %ld from %@", code, url.absoluteString];
+    XCTAssertEqualObjects(expectedErrMsg, err.localizedDescription);
+    err = nil;
+  }
+}
+
+- (void)testEnsureErrorsFromTransportAreHandled {
+  NSURL *url = [NSURL URLWithString:@"http://localhost:9444"];
+  NSError *mockErr = [[NSError alloc] initWithDomain:@"com.google.santa.metricservice.writers.http"
+                                                code:505
+                                            userInfo:@{NSLocalizedDescriptionKey : @"test error"}];
+  NSError *err;
+
+  [self createMockResponseWithURL:url withCode:505 withData:nil withError:mockErr];
+
+  NSData *JSONdata = [@"{\"foo\": \"bar\"}\r\n" dataUsingEncoding:NSUTF8StringEncoding];
+
+  BOOL result = [self.httpWriter write:@[ JSONdata ] toURL:url error:&err];
+
+  XCTAssertEqual(NO, result, @"result of call to write did not fail as expected");
+  XCTAssertEqual(mockErr.code, err.code);
+  XCTAssertEqualObjects(mockErr.domain, err.domain);
+  XCTAssertEqualObjects(@"test error", err.userInfo[NSLocalizedDescriptionKey]);
+}
+
+- (void)testEnsureMutlipleEntriesWriteMultipleTimes {
+  NSURL *url = [NSURL URLWithString:@"http://localhost:9444"];
+
+  // Ensure that non-200 status codes codes do not crash
+  [self createMockResponseWithURL:url withCode:200 withData:nil withError:nil];
+  [self createMockResponseWithURL:url withCode:200 withData:nil withError:nil];
+
+  NSData *JSONdata = [@"{\"foo\": \"bar\"}\r\n" dataUsingEncoding:NSUTF8StringEncoding];
+  NSError *err;
+  BOOL result = [self.httpWriter write:@[ JSONdata, JSONdata ] toURL:url error:&err];
+
+  XCTAssertEqual(YES, result);
+  XCTAssertNil(err);
+  XCTAssertEqual(0, self.mockResponses.count, @"incorrect number of requests was made");
+}
+
+- (void)testEnsurePassingNilOrNullErrorDoesNotCrash {
+  NSURL *url = [NSURL URLWithString:@"http://localhost:9444"];
+
+  // Queue up two responses for nil and NULL.
+  [self createMockResponseWithURL:url withCode:400 withData:nil withError:nil];
+  [self createMockResponseWithURL:url withCode:400 withData:nil withError:nil];
+
+  NSData *JSONdata = [@"{\"foo\": \"bar\"}\r\n" dataUsingEncoding:NSUTF8StringEncoding];
+
+  BOOL result = [self.httpWriter write:@[ JSONdata ] toURL:url error:nil];
+  XCTAssertEqual(NO, result);
+
+  result = [self.httpWriter write:@[ JSONdata ] toURL:url error:NULL];
+  XCTAssertEqual(NO, result);
+
+  NSError *mockErr =
+    [[NSError alloc] initWithDomain:@"com.google.santa.metricservice.writers.http.test"
+                               code:505
+                           userInfo:@{NSLocalizedDescriptionKey : @"test error"}];
+
+  // Queue up two responses for nil and NULL.
+  [self createMockResponseWithURL:url withCode:500 withData:nil withError:mockErr];
+  [self createMockResponseWithURL:url withCode:500 withData:nil withError:mockErr];
+
+  result = [self.httpWriter write:@[ JSONdata ] toURL:url error:nil];
+  XCTAssertFalse(result);
+
+  result = [self.httpWriter write:@[ JSONdata ] toURL:url error:NULL];
+
+  XCTAssertFalse(result);
+}
+@end
