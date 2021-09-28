67707996046a0c3c4ccc262f586c5753a8d1d46c
Kent Ma <tnek@google.com>
2021-09-28 12:42:36 -0400

    Create a local keychain instead of using the system keychain and drop sudo from most of the build stages

--- Testing/build_and_sign.sh
+++ Testing/build_and_sign.sh
@@ -2,6 +2,7 @@
 set -e
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
+KEYCHAIN="santa-dev-test.keychain"
 SANTAD_PATH=Santa.app/Contents/Library/SystemExtensions/com.google.santa.daemon.systemextension/Contents/MacOS/com.google.santa.daemon
 SANTAD_ENTITLEMENTS="$GIT_ROOT/Source/santad/com.google.santa.daemon.systemextension.entitlements"
 SANTA_BIN_PATH=Santa.app/Contents/MacOS
@@ -9,7 +10,7 @@ SIGNING_IDENTITY="localhost"
 
 
 function main() {
-    sudo bazel build --ios_signing_cert_name=$SIGNING_IDENTITY --apple_generate_dsym -c opt --define=SANTA_BUILD_TYPE=ci --define=apple.propagate_embedded_extra_outputs=yes --macos_cpus=x86_64,arm64 //:release
+    bazel build --apple_generate_dsym -c opt --define=SANTA_BUILD_TYPE=ci --define=apple.propagate_embedded_extra_outputs=yes --macos_cpus=x86_64,arm64 //:release
 
     echo "> Build complete, installing santa"
     TMP_DIR=$(mktemp -d)
@@ -17,10 +18,10 @@ function main() {
     CS_ARGS="--prefix=EQHXZ8M8AV -fs $SIGNING_IDENTITY --timestamp --options library,kill,runtime"
 
     for bin in $TMP_DIR/binaries/$SANTA_BIN_PATH/*; do
-        sudo codesign --preserve-metadata=entitlements ${CS_ARGS} $bin
+        codesign --keychain $KEYCHAIN --preserve-metadata=entitlements ${CS_ARGS} $bin
     done
 
-    sudo codesign ${CS_ARGS} --entitlements $SANTAD_ENTITLEMENTS $TMP_DIR/binaries/$SANTAD_PATH
+    codesign ${CS_ARGS} --keychain $KEYCHAIN --entitlements $SANTAD_ENTITLEMENTS $TMP_DIR/binaries/$SANTAD_PATH
 
     echo "> Running install.sh"
     (
--- Testing/reset.sh
+++ Testing/reset.sh
@@ -3,3 +3,4 @@ killall moroz
 security delete-identity -c "localhost"
 rm -rf /Applications/Santa.app
 systemextensionsctl reset
+security delete-keychain santa-dev-test.keychain
