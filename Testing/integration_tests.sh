3e9add6585f4e4c703cf315627a111902b61ee41
Kent Ma <tnek@google.com>
2021-10-07 10:19:30 -0400

    Use direct path in integration_tests.sh instead of relative path

--- Testing/integration_tests.sh
+++ Testing/integration_tests.sh
@@ -2,15 +2,18 @@
 set -e
 set -x
 
+GIT_ROOT=$(git rev-parse --show-toplevel)
 
 run_tests() {
-    local -a TEST_FLAGS=( --strategy=TestRunner=standalone --test_output=all )
-    cd ./integration
-    time bazel test "${TEST_FLAGS[@]}" -- ...
+    (
+        local -a TEST_FLAGS=( --strategy=TestRunner=standalone --test_output=all )
+        cd $GIT_ROOT/Testing/integration
+        time bazel test "${TEST_FLAGS[@]}" -- ...
+    )
 }
 
 setup() {
-    ./start_env.sh
+    $GIT_ROOT/Testing/start_env.sh
     sudo santactl sync --debug
 }
 
