8b3f005b432c572b3fa78c5239796d48d5ccbbd1
Kent Ma <tnek@google.com>
2021-10-11 11:30:37 -0400

    Remove set -e, instead just propagate the error up

--- Testing/lint.sh
+++ Testing/lint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -eu
 
 function main() {
+    err=0
     GIT_ROOT=$(git rev-parse --show-toplevel)
 
     find $GIT_ROOT \( -name "*.m" -o -name "*.h" -name "*.mm" \) | xargs clang-format --Werror --dry-run
+    err="$(( $err | $? ))"
 
     go get github.com/bazelbuild/buildtools/buildifier
     ~/go/bin/buildifier --lint=warn -r $GIT_ROOT
+    err="$(( $err | $? ))"
+    return $err
 }
 
 main $@
