1ec70850be59e25af367105fa3f8eeaa0cdba125
Kent Ma <kent@osiris.cyber.nyu.edu>
2021-10-11 11:11:25 -0400

    Apply suggestions from code review
    Co-authored-by: Russell Hancox <russellhancox@users.noreply.github.com>

--- Testing/lint.sh
+++ Testing/lint.sh
@@ -4,7 +4,7 @@ set -eu
 function main() {
     GIT_ROOT=$(git rev-parse --show-toplevel)
 
-    find $GIT_ROOT \( -name "*.m" -o -name "*.h" -name "*.mm" \) | xargs clang-format --Werror --dry-run
+    find $GIT_ROOT \( -name "*.m" -o -name "*.h" -name "*.mm" \) -exec clang-format --Werror --dry-run {} \+
 
     go get github.com/bazelbuild/buildtools/buildifier
     ~/go/bin/buildifier --lint=warn -r $GIT_ROOT
