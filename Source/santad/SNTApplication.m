54f977b723f7354a95f1373a75eacc7b3af74e62
Pete Markowsky <markowsky@google.com>
2021-10-25 17:23:57 -0400

    Adding spaces that keep getting removed by clang-format back.

--- Source/santad/SNTApplication.m
+++ Source/santad/SNTApplication.m
@@ -408,7 +408,7 @@ dispatch_source_t createDispatchTimer(uint64_t interval, uint64_t leeway, dispat
     }
   } else if ([keyPath isEqualToString:NSStringFromSelector(@selector(exportMetrics))]) {
     BOOL new = [ change[newKey] boolValue ];
-    BOOL old = [change[oldKey] boolValue];
+    BOOL old = [ change[oldKey] boolValue ];
 
     if (old == NO && new == YES) {
       LOGI(@"metricsExport changed NO -> YES, starting to export metrics");
