eb8112c801d5b14d186ae2f740a24db7adda595a
Pete Markowsky <markowsky@google.com>
2021-11-15 15:12:06 -0500

    Updated unit test and metrics. Ran clang-format.

--- Source/santad/SNTApplicationCoreMetrics.m
+++ Source/santad/SNTApplicationCoreMetrics.m
@@ -88,7 +88,6 @@ static void RegisterHostnameAndUsernameLabels(SNTMetricSet *metricSet) {
   [metricSet addRootLabel:@"host_name" value:[NSProcessInfo processInfo].hostName];
   [metricSet addRootLabel:@"username" value:NSUserName()];
   [metricSet addRootLabel:@"job_name" value:@"santad"];
-  [metricSet addRootLabel:@"corp_site" value:@"roam"];
 }
 
 static void RegisterCommonSantaMetrics(SNTMetricSet *metricSet) {
