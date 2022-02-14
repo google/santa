faa8946056a9411ef3b98662cfe6787f6590f0fe
Pete Markowsky <pmarkowsky@users.noreply.github.com>
2022-02-14 13:51:29 -0500

    Fix: remediate a crash in santametricservice (#729)
    * Fix issue with task cancelation.
    
    * Make export timeouts configurable.
    
    This allows an export timeout to be set via configuration and eases testing.

--- Source/common/SNTConfigurator.m
+++ Source/common/SNTConfigurator.m
@@ -110,6 +110,7 @@ static NSString *const kBlockedPathRegexKeyDeprecated = @"BlacklistRegex";
 static NSString *const kMetricFormat = @"MetricFormat";
 static NSString *const kMetricURL = @"MetricURL";
 static NSString *const kMetricExportInterval = @"MetricExportInterval";
+static NSString *const kMetricExportTimeout = @"MetricExportTimeout";
 static NSString *const kMetricExtraLabels = @"MetricExtraLabels";
 
 // The keys managed by a sync server.
@@ -193,6 +194,7 @@ static NSString *const kSyncCleanRequired = @"SyncCleanRequired";
       kMetricFormat : string,
       kMetricURL : string,
       kMetricExportInterval : number,
+      kMetricExportTimeout : number,
       kMetricExtraLabels : dictionary,
     };
     _defaults = [NSUserDefaults standardUserDefaults];
@@ -752,6 +754,16 @@ static NSString *const kSyncCleanRequired = @"SyncCleanRequired";
 // Returns a default value of 30 (for 30 seconds).
 - (NSUInteger)metricExportInterval {
   NSNumber *configuredInterval = self.configState[kMetricExportInterval];
+
+  if (configuredInterval == nil) {
+    return 30;
+  }
+  return [configuredInterval unsignedIntegerValue];
+}
+
+// Returns a default value of 30 (for 30 seconds).
+- (NSUInteger)metricExportTimeout {
+  NSNumber *configuredInterval = self.configState[kMetricExportTimeout];
 
   if (configuredInterval == nil) {
     return 30;
