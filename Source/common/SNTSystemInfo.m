b433d07339b71a4615c59b9601acf701ea43f8e0
Éric Falconnier <eric@zentral.pro>
2022-03-15 13:27:15 +0100

    Fix clang-format error

--- Source/common/SNTSystemInfo.m
+++ Source/common/SNTSystemInfo.m
@@ -12,8 +12,8 @@
 ///    See the License for the specific language governing permissions and
 ///    limitations under the License.
 
-#import <sys/utsname.h>
 #import "Source/common/SNTSystemInfo.h"
+#import <sys/utsname.h>
 
 @implementation SNTSystemInfo
 
