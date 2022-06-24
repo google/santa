a7d684eb70f070d9a63e8d20fc06a05ff1fde5c7
Matt White <436037+mlw@users.noreply.github.com>
2022-06-24 16:48:04 -0400

    Update copyright header blobs, convert some tabs to spaces

--- Source/santad/EventProviders/EndpointSecurity/Client.h
+++ Source/santad/EventProviders/EndpointSecurity/Client.h
@@ -1,3 +1,17 @@
+/// Copyright 2022 Google Inc. All rights reserved.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///    http://www.apache.org/licenses/LICENSE-2.0
+///
+///    Unless required by applicable law or agreed to in writing, software
+///    distributed under the License is distributed on an "AS IS" BASIS,
+///    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///    See the License for the specific language governing permissions and
+///    limitations under the License.
+
 #ifndef SANTA__SANTAD__EVENTPROVIDERS_ENDPOINTSECURITY_CLIENT_H
 #define SANTA__SANTAD__EVENTPROVIDERS_ENDPOINTSECURITY_CLIENT_H
 
@@ -9,36 +23,37 @@ namespace santa::santad::event_providers::endpoint_security {
 
 class Client {
 public:
-	explicit Client(es_client_t* client,
-                                  es_new_client_result_t result)
+  explicit Client(es_client_t* client,
+                  es_new_client_result_t result)
       : client_(client), result_(result) {}
+
   explicit Client(std::nullptr_t c)
       : client_(nullptr), result_(ES_NEW_CLIENT_RESULT_ERR_INTERNAL) {}
-  Client()
-      : client_(nullptr), result_(ES_NEW_CLIENT_RESULT_ERR_INTERNAL) {}
 
-	virtual ~Client() {
-		if (client_) {
-			es_delete_client(client_);
-		}
-	}
+  Client() : client_(nullptr), result_(ES_NEW_CLIENT_RESULT_ERR_INTERNAL) {}
 
-	Client(Client&& other) {
-		client_ = other.client_;
+  virtual ~Client() {
+    if (client_) {
+      es_delete_client(client_);
+    }
+  }
+
+  Client(Client&& other) {
+    client_ = other.client_;
     result_ = other.result_;
-		other.client_ = nullptr;
+    other.client_ = nullptr;
     other.result_ = ES_NEW_CLIENT_RESULT_ERR_INTERNAL;
-	}
+  }
 
-	void operator=(Client&& rhs) {
+  void operator=(Client&& rhs) {
     client_ = rhs.client_;
     result_ = rhs.result_;
     rhs.client_ = nullptr;
     rhs.result_ = ES_NEW_CLIENT_RESULT_ERR_INTERNAL;
   }
 
-	Client(const Client& other) = delete;
-	void operator=(const Client& rhs) = delete;
+  Client(const Client& other) = delete;
+  void operator=(const Client& rhs) = delete;
 
   bool IsConnected() {
     return client_ != nullptr && result_ == ES_NEW_CLIENT_RESULT_SUCCESS;
@@ -48,7 +63,7 @@ public:
   es_client_t* Get() const { return client_; }
 
 private:
-	es_client_t *client_;
+  es_client_t *client_;
   es_new_client_result_t result_;
 };
 
