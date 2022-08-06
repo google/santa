67c3c2c3711c82eba4a07f3c475cffb882662163
Matt White <436037+mlw@users.noreply.github.com>
2022-08-05 22:07:23 -0400

    Properly mock EndpointSecurityAPI

--- Source/santad/EventProviders/AuthResultCacheTest.mm
+++ Source/santad/EventProviders/AuthResultCacheTest.mm
@@ -15,17 +15,19 @@
 #include <EndpointSecurity/EndpointSecurity.h>
 #include <Foundation/Foundation.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <memory>
 
 #include "Source/common/SNTCommon.h"
 #include "Source/santad/EventProviders/AuthResultCache.h"
+#include "Source/santad/EventProviders/EndpointSecurity/Client.h"
 #include "Source/santad/EventProviders/EndpointSecurity/EndpointSecurityAPI.h"
 
 using santa::santad::event_providers::AuthResultCache;
 using santa::santad::event_providers::FlushCacheMode;
+using santa::santad::event_providers::endpoint_security::Client;
 using santa::santad::event_providers::endpoint_security::EndpointSecurityAPI;
-
 
 // Grab the st_dev number of the root volume to match the root cache
 static uint64_t RootDevno() {
@@ -66,7 +68,6 @@ public:
   // AuthResultCacheTest(std::shared_ptr<EndpointSecurityAPI> es_api)
   //   : AuthResultCache(std::move(es_api)) {}
 
-
   auto RootCache() { return root_cache_; }
   auto NonRootCache() { return nonroot_cache_; }
 };
@@ -87,15 +88,20 @@ static inline void ExpectCacheCounts(std::shared_ptr<AuthResultCache> cache,
               [counts[1] unsignedLongLongValue] == nonroot_count);
 }
 
+class MockEndpointSecurityAPI : public EndpointSecurityAPI {
+  public:
+    MOCK_METHOD(bool, ClearCache, (const Client &client));
+};
+
 TEST(AuthResultCache, EmptyCacheExpectedNumberOfCacheCounts) {
-  auto esapi = std::make_shared<EndpointSecurityAPI>();
+  auto esapi = std::make_shared<MockEndpointSecurityAPI>();
   auto cache = std::make_shared<AuthResultCache>(esapi);
 
   ExpectCacheCounts(cache, 0, 0);
 }
 
 TEST(AuthResultCache, BasicOperation) {
-  auto esapi = std::make_shared<EndpointSecurityAPI>();
+  auto esapi = std::make_shared<MockEndpointSecurityAPI>();
   auto cache = std::make_shared<AuthResultCacheTest>(esapi);
 
   es_file_t root_file = MakeCacheableFile(RootDevno(), 111);
@@ -132,10 +138,8 @@ TEST(AuthResultCache, BasicOperation) {
 }
 
 TEST(AuthResultCache, FlushCache) {
-  auto esapi = std::make_shared<EndpointSecurityAPI>();
-  auto cache = std::make_shared<AuthResultCache>(esapi);
-
-  printf("Just a print to find the function...\n");
+  auto mock_esapi = std::make_shared<MockEndpointSecurityAPI>();
+  auto cache = std::make_shared<AuthResultCache>(mock_esapi);
 
   es_file_t root_file = MakeCacheableFile(RootDevno(), 111);
   es_file_t nonroot_file = MakeCacheableFile(RootDevno() + 123, 111);
@@ -156,12 +160,24 @@ TEST(AuthResultCache, FlushCache) {
   ExpectCacheCounts(cache, 1, 1);
 
   // Flush all caches
-  //
-  // TODO: EndpointSecurityAPI must be made injectable since kAllCaches will call into es_clear_cache
-  //
-  // cache->FlushCache(FlushCacheMode::kAllCaches);
+  // The call to ClearCache is asynchronous. Use a semaphore to
+  // be notified when the mock is called.
+  dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+  EXPECT_CALL(*mock_esapi, ClearCache(testing::_))
+      .WillOnce(testing::InvokeWithoutArgs(^() {
+          dispatch_semaphore_signal(sema);
+          return true;
+      }));
+  cache->FlushCache(FlushCacheMode::kAllCaches);
 
-  // ExpectCacheCounts(cache, 0, 0);
+  ASSERT_EQ(
+      0,
+      dispatch_semaphore_wait(
+          sema,
+          dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC)))
+      << "ClearCache wasn't called within expected window";
+
+  ExpectCacheCounts(cache, 0, 0);
 }
 
 // TEST(AddToCache, CacheStateMachine) {
