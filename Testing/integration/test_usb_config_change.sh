57e57feb45ff6b5fb98bdbfc414b6e42845751bc
Nick Gregory <nickmg@google.com>
2022-08-25 17:35:50 -0400

    bazelify install_profile

--- Testing/integration/test_usb_config_change.sh
+++ Testing/integration/test_usb_config_change.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ueo pipefail
 
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
 if [[ "$(santactl status --json | jq .daemon.block_usb)" != "false" ]]; then
   echo "USB blocking already enabled?" >&2
   exit 1
 fi
 
-osascript Testing/integration/install_profile.scpt Testing/integration/configs/usb-block.mobileconfig
+bazel run //Testing/integration:install_profile -- Testing/integration/configs/usb-block.mobileconfig
 sleep 1
 
 if [[ "$(santactl status --json | jq .daemon.block_usb)" != "true" ]]; then
