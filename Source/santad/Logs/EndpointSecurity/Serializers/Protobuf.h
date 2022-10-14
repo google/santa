e71b51894d025086c1b6fa60690c49da2f6bb773
Matt White <436037+mlw@users.noreply.github.com>
2022-10-13 20:19:42 -0400

    Import foundation so the linter doesn't complain

--- Source/santad/Logs/EndpointSecurity/Serializers/Protobuf.h
+++ Source/santad/Logs/EndpointSecurity/Serializers/Protobuf.h
@@ -15,6 +15,8 @@
 #ifndef SANTA__SANTAD__LOGS_ENDPOINTSECURITY_SERIALIZERS_PROTOBUF_H
 #define SANTA__SANTAD__LOGS_ENDPOINTSECURITY_SERIALIZERS_PROTOBUF_H
 
+#import <Foundation/Foundation.h>
+
 #include <memory>
 #include <vector>
 
