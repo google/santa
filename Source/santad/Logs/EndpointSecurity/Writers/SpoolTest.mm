e69c00c33bfad099a822de126c1c64e93f851e90
Matt White <436037+mlw@users.noreply.github.com>
2022-10-19 10:43:36 -0400

    Add spool writer tests

--- Source/santad/Logs/EndpointSecurity/Writers/SpoolTest.mm
+++ Source/santad/Logs/EndpointSecurity/Writers/SpoolTest.mm
@@ -13,12 +13,17 @@
 /// limitations under the License.
 
 #import <Foundation/Foundation.h>
+#include <sys/_types/_useconds_t.h>
 #import <XCTest/XCTest.h>
 #include <dispatch/dispatch.h>
+#include <gtest/gtest.h>
+#include <unistd.h>
 #include <memory>
-#include "Source/common/TestUtils.h"
 
-#import "Source/santad/Logs/EndpointSecurity/Writers/Spool.h"
+#include "Source/common/TestUtils.h"
+#include "Source/santad/Logs/EndpointSecurity/Writers/FSSpool/fsspool.h"
+#include "Source/santad/Logs/EndpointSecurity/Writers/FSSpool/fsspool_log_batch_writer.h"
+#include "Source/santad/Logs/EndpointSecurity/Writers/Spool.h"
 
 namespace santa::santad::logs::endpoint_security::writers {
 
@@ -32,27 +37,100 @@ class SpoolPeer : public Spool {
 
 }  // namespace santa::santad::logs::endpoint_security::writers
 
+using ::fsspool::FsSpoolLogBatchWriter;
+using ::fsspool::FsSpoolWriter;
 using santa::santad::logs::endpoint_security::writers::Spool;
 using santa::santad::logs::endpoint_security::writers::SpoolPeer;
 
 @interface SpoolTest : XCTestCase
 @property dispatch_queue_t q;
 @property dispatch_source_t timer;
+@property NSFileManager *fileMgr;
+@property NSString *testDir;
+@property NSString *baseDir;
+@property NSString *spoolDir;
 @end
 
 @implementation SpoolTest
 
 - (void)setUp {
+  self.fileMgr = [NSFileManager defaultManager];
+  self.testDir = [NSString stringWithFormat:@"%@santa-spool-%d", NSTemporaryDirectory(), getpid()];
+  self.testDir = [NSString stringWithFormat:@"%@fsspool-%d", NSTemporaryDirectory(), getpid()];
+  self.baseDir = [NSString stringWithFormat:@"%@/base", self.testDir];
+  self.spoolDir = [NSString stringWithFormat:@"%@/new", self.baseDir];
+
+  XCTAssertFalse([self.fileMgr fileExistsAtPath:self.baseDir]);
+  XCTAssertFalse([self.fileMgr fileExistsAtPath:self.spoolDir]);
+
+  XCTAssertTrue([self.fileMgr createDirectoryAtPath:self.testDir
+                        withIntermediateDirectories:YES
+                                         attributes:nil
+                                              error:nil]);
+
   self.q = dispatch_queue_create(NULL, DISPATCH_QUEUE_SERIAL);
   XCTAssertNotNil(self.q);
   self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, self.q);
   XCTAssertNotNil(self.timer);
 }
 
+- (void)tearDown {
+  XCTAssertTrue([self.fileMgr removeItemAtPath:self.testDir error:nil]);
+}
+
 - (void)testTypeUrl {
-  auto spool = std::make_shared<SpoolPeer>("/tmp", 1, 1, 1, self.q, self.timer);
+  // Ensure the manually created type url isn't modified
+  auto spool =
+    std::make_shared<SpoolPeer>(self.q, self.timer, [self.baseDir UTF8String], 10240, 1024);
   std::string wantTypeUrl("type.googleapis.com/santa.pb.v1.SantaMessage");
   XCTAssertCppStringEqual(spool->GetTypeUrl(), wantTypeUrl);
+}
+
+- (void)testWrite {
+  const useconds_t waitTime = 1000000; // 1.0 seconds
+  const useconds_t shortWaitTime = 250000; // 0.25 seconds
+  const size_t writeSize = 50;
+  const uint64 periodicFlushMS = 400;
+  NSError *err = nil;
+
+  auto spool =
+    std::make_shared<SpoolPeer>(self.q, self.timer, [self.baseDir UTF8String], 10240, 1024);
+
+  // Set a custom timer interval for this test
+  dispatch_source_set_timer(self.timer, dispatch_time(DISPATCH_TIME_NOW, 0), NSEC_PER_MSEC * periodicFlushMS,
+                            0);
+
+  spool->Write(std::vector<uint8_t>(writeSize, 'A'));
+
+  // Sleep for 1.25 seconds
+  usleep(waitTime);
+
+  // Ensure nothing exists yet because periodic flush been started
+  XCTAssertEqual([[self.fileMgr contentsOfDirectoryAtPath:self.spoolDir error:&err] count], 0);
+
+  // Manual Flush
+  XCTAssertTrue(spool->Flush());
+
+  // A new log entry should exist
+  XCTAssertEqual([[self.fileMgr contentsOfDirectoryAtPath:self.spoolDir error:&err] count], 1);
+
+  // Start the periodic flush task
+  spool->BeginFlushTask();
+
+  // Do a short wait to let the timer do the initial fire
+  usleep(shortWaitTime);
+
+  // Ensure no growth in the amount of data
+  XCTAssertEqual([[self.fileMgr contentsOfDirectoryAtPath:self.spoolDir error:&err] count], 1);
+
+  // Write a second log entry and begin the period
+  spool->Write(std::vector<uint8_t>(writeSize, 'B'));
+
+  // Wait for the periodic task to run
+  usleep(waitTime);
+
+  // Ensure the new log entry appears
+  XCTAssertEqual([[self.fileMgr contentsOfDirectoryAtPath:self.spoolDir error:&err] count], 2);
 }
 
 @end
