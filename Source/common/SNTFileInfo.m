c64f6879217b47fb64bbaa8518404a4e49db029d
Nick Gregory <computerfreak97@gmail.com>
2022-12-27 15:45:57 -0500

    __builtin_add_overflow
    Co-authored-by: Matt W <436037+mlw@users.noreply.github.com>

--- Source/common/SNTFileInfo.m
+++ Source/common/SNTFileInfo.m
@@ -642,9 +642,10 @@ extern NSString *const NSURLQuarantinePropertiesKey WEAK_IMPORT_ATTRIBUTE;
 ///
 - (NSData *)safeSubdataWithRange:(NSRange)range {
   @try {
-    if (range.location > self.fileSize || range.length > self.fileSize ||
-        (range.location + range.length) > self.fileSize)
+    NSUInteger size;
+    if (__builtin_add_overflow(range.location, range.length, &size) || size > self.fileSize) {
       return nil;
+    }
     [self.fileHandle seekToFileOffset:range.location];
     NSData *d = [self.fileHandle readDataOfLength:range.length];
     if (d.length != range.length) return nil;
