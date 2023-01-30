e1ce448be10618bf44639eff7192778b51a5abeb
Matt White <436037+mlw@users.noreply.github.com>
2023-01-30 11:59:37 -0500

    Remove copy/paste

--- Source/santad/Logs/EndpointSecurity/Serializers/Protobuf.mm
+++ Source/santad/Logs/EndpointSecurity/Serializers/Protobuf.mm
@@ -1,4 +1,4 @@
-Source/santad/Logs/EndpointSecurity/Serializers/Protobuf.mm/// Copyright 2022 Google LLC
+/// Copyright 2022 Google LLC
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
