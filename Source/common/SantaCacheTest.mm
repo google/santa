1624a42ea76243961415f21b3f9a2b6487f79fb4
Matt White <436037+mlw@users.noreply.github.com>
2023-11-29 16:13:56 -0500

    Fix test issue caused by move to C++20

--- Source/common/SantaCacheTest.mm
+++ Source/common/SantaCacheTest.mm
@@ -245,7 +245,7 @@ struct S {
   uint64_t first_val;
   uint64_t second_val;
 
-  bool operator==(const S &rhs) {
+  bool operator==(const S &rhs) const {
     return first_val == rhs.first_val && second_val == rhs.second_val;
   }
 };
