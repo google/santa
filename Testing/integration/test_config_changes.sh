1596990c659db01a4c9cfa9cca2c487f89d4c775
Nick Gregory <nickmg@google.com>
2023-12-04 13:01:30 -0500

    reorder e2e tests (#1249)

--- Testing/integration/test_config_changes.sh
+++ Testing/integration/test_config_changes.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -x
 
-# TODO(nickmg): These `santactl status`s should be run with sudo to mirror the others,
-# however currently (2022-10-27) non-root status is what correctly reads from provisioning profile configuration.
-
 bazel run //Testing/integration:install_profile -- Testing/integration/configs/default.mobileconfig
 if [[ "$(santactl status --json | jq .daemon.block_usb)" != "false" ]]; then
   echo "USB blocking enabled with minimal config" >&2
