0798853fd5a639d33b5277d49ed5aa15a6baf5d1
Adam Sindelar <adam@wowsignal.io>
2023-12-15 10:38:35 +0100

    Improve the e2e test to validate the file contents

--- Source/santad/Logs/EndpointSecurity/ParquetLogger/e2e_test/check_parquet_file.py
+++ Source/santad/Logs/EndpointSecurity/ParquetLogger/e2e_test/check_parquet_file.py
@@ -3,9 +3,20 @@
 import pandas as pd
 import sys
 
-df = pd.read_parquet(sys.argv[1])
-if len(df) != 4:
-    print("Expected 4 rows, got %d" % len(df))
-    sys.exit(1)
+def expect_eq(got, want):
+    if got != want:
+        print(f"Expected {want}, got {got}")
+        sys.exit(1)
 
+df = pd.read_parquet(sys.argv[1])
+expect_eq(len(df), 4)
+
+# Strings should be stored correctly and decode as binary strings (not UTF-8).
+expect_eq(df.to_dict()["text"][0], b"Hello, world!")
+expect_eq(df.to_dict()["text"][3], b"Good bye, world!")
+
+# Check that numbers are encoded correctly.
+expect_eq(df.to_dict()["big_number"][2], 0xcafed00d)
+
+print("[OK] - example parquet file is valid")
 sys.exit(0)
--- Source/santad/Logs/EndpointSecurity/ParquetLogger/write_test_file.cc
+++ Source/santad/Logs/EndpointSecurity/ParquetLogger/write_test_file.cc
@@ -6,15 +6,17 @@
 // implemented in Rust. The C++ API uses an opaque type called Table, which is
 // returned as a Box (effectively a unique_ptr).
 //
-// To easily check that the file is valid, load it into Pandas:
-//
-// import pandas as pd; pd.read_parquet("./test_table.parquet")
+// To easily check that the file is valid, run the e2e_test in the same
+// directory.
 int main(int argc, char* argv[]) {
   try {
     if (argc < 2) {
       std::cout << "Usage: " << argv[0] << " <path>" << std::endl;
       return 1;
     }
+
+    // IMPORTANT: If you change anything about the file, you MUST ALSO change
+    // check_parquet_file.py, otherwise the e2e test will break.
 
     // Make me a table builder:
     auto args = pedro::wire::table_args_new("test_table", argv[1]);
@@ -40,7 +42,7 @@ int main(int argc, char* argv[]) {
 
     pedro::wire::table_push_string(*table, 1, "Hello, world!");
     pedro::wire::table_push_string(*table, 1, "Hello, world!");
-    pedro::wire::table_push_string(*table, 1, "Hello, world!");
+    pedro::wire::table_push_string(*table, 1, "Good bye, world!");
 
     pedro::wire::table_push_i64(*table, 2, 0xdeadbeef);
     pedro::wire::table_push_i64(*table, 2, 0xcafed00d);
@@ -53,9 +55,9 @@ int main(int argc, char* argv[]) {
     // buffer. To finalize the parquet file, we need to destroy the table.
     pedro::wire::table_end(std::move(table));
 
-    std::cout << "Success!" << std::endl;
+    std::cout << "Successfully wrote the example parquet file " << argv[0] << std::endl;
   } catch (std::exception& e) {
-    std::cout << "Error!" << std::endl;
+    std::cout << "Error writing parquet file:" << std::endl;
     std::cout << e.what() << std::endl;
   }
   return 0;
