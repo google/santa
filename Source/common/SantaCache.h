6680f9031c27a3c5d654cfa1f322fd5302357234
Nick Gregory <nickmg@google.com>
2024-02-20 15:06:28 -0500

    more lint

--- Source/common/SantaCache.h
+++ Source/common/SantaCache.h
@@ -320,7 +320,8 @@ class SantaCache {
     Lock a bucket. Spins until the lock is acquired.
   */
   inline void lock(struct bucket *bucket) const {
-    while (OSAtomicTestAndSet(7, (volatile uint8_t *)&bucket->head));
+    while (OSAtomicTestAndSet(7, (volatile uint8_t *)&bucket->head)) {
+    }
   }
 
   /**
