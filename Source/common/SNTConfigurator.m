9bef9b5a7349b3fedc09a73ec1e22b4f50a1946e
Russell Hancox <russell@hancox.us>
2024-05-31 14:32:25 -0400

    sync: Add option to sync using binary protos

--- Source/common/SNTConfigurator.m
+++ Source/common/SNTConfigurator.m
@@ -73,6 +73,7 @@ static NSString *const kMobileConfigDomain = @"com.google.santa";
 /// The keys managed by a mobileconfig.
 static NSString *const kStaticRules = @"StaticRules";
 static NSString *const kSyncBaseURLKey = @"SyncBaseURL";
+static NSString *const kSyncEnableProtoTransfer = @"SyncEnableProtoTransfer";
 static NSString *const kSyncProxyConfigKey = @"SyncProxyConfiguration";
 static NSString *const kSyncExtraHeadersKey = @"SyncExtraHeaders";
 static NSString *const kSyncEnableCleanSyncEventUpload = @"SyncEnableCleanSyncEventUpload";
@@ -234,6 +235,7 @@ static NSString *const kSyncTypeRequired = @"SyncTypeRequired";
       kModeNotificationLockdown : string,
       kStaticRules : array,
       kSyncBaseURLKey : string,
+      kSyncEnableProtoTransfer : number,
       kSyncEnableCleanSyncEventUpload : number,
       kSyncProxyConfigKey : dictionary,
       kSyncExtraHeadersKey : dictionary,
@@ -717,6 +719,11 @@ static NSString *const kSyncTypeRequired = @"SyncTypeRequired";
   if (![urlString hasSuffix:@"/"]) urlString = [urlString stringByAppendingString:@"/"];
   NSURL *url = [NSURL URLWithString:urlString];
   return url;
+}
+
+- (BOOL)syncEnableProtoTransfer {
+  NSNumber *number = self.configState[kSyncEnableProtoTransfer];
+  return number ? [number boolValue] : NO;
 }
 
 - (NSDictionary *)syncProxyConfig {
